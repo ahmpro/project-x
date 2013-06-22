9089ef1e2671fcd737b536bd57229f5287cf9f30
Vladislav Klimanov <ahmpro@rslayers.com>
2013-06-23 03:30:47 +0400

    rewrite work with keyboard: more flexible and look pretty nice

--- client/js/io.js
+++ client/js/io.js
@@ -1,93 +1,64 @@
 var IO = Backbone.Model.extend({
-    flags: {
-        move: {
-            up: 0,
-            down: 0,
-            left: 0,
-            right: 0
-        },
+    keyboard: {
+        flag: {},
 
-        move_stop: {
-            up: 0,
-            down: 0,
-            left: 0,
-            right: 0
+        keys : {
+            'W': 87, 'A': 65, 'S': 83, 'D': 68,
+            'arrow up': 38, 'arrow down': 40,
+            'arrow left': 37, 'arrow right': 39,
         },
     },
 
     keydown: function(e) {
-        var key = e.keyCode;
-        switch (key){
-            case 87:
-                this.flags.move.up = 1;
-                break;
-            case 65:
-                this.flags.move.left = 1;
-                break;
-            case 83:
-                this.flags.move.down = 1;
-                break;
-            case 68:
-                this.flags.move.right = 1;
-                break;
-            //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-            case 38:
-                this.trigger('player_direct', 'up');
-                break;
-            case 37:
-                this.trigger('player_direct', 'left');
-                break;
-            case 40:
-                this.trigger('player_direct', 'down');
-                break;
-            case 39:
-                this.trigger('player_direct', 'right');
-                break;
+        var keyCode = e.keyCode;
+
+        if (this.keyboard.flag[''+keyCode]) {
+            this.keyboard.flag[''+keyCode].free = false;
+        } else {
+            this.keyboard.flag[''+keyCode] = {
+                free: false,
+                keyCode: keyCode,
+            };
         }
     },
 
     keyup: function(e) {
-        var key = e.keyCode;
-        switch (key){
-            case 87:
-                this.flags.move.up = 0;
-                this.flags.move_stop.up = 1;
-                break;
-            case 65:
-                this.flags.move.left = 0;
-                this.flags.move_stop.left = 1;
-                break;
-            case 83:
-                this.flags.move.down = 0;
-                this.flags.move_stop.down = 1;
-                break;
-            case 68:
-                this.flags.move.right = 0;
-                this.flags.move_stop.right = 1;
-                break;
-        }
+        this.keyboard.flag[''+e.keyCode].free = true;
     },
 
     tick: function() {
-        // implementation here
-        if (this.flags.move.up||this.flags.move_stop.up) {
-            this.trigger('player_move', 'up');
-        };
-        if (this.flags.move.down||this.flags.move_stop.down){
-            this.trigger('player_move', 'down');
-        };
-        if (this.flags.move.left||this.flags.move_stop.left){
-            this.trigger('player_move', 'left');
-        };
-        if (this.flags.move.right||this.flags.move_stop.right) {
-            this.trigger('player_move', 'right');
-        };
-        //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-        this.flags.move_stop.up = 0;
-        this.flags.move_stop.down = 0;
-        this.flags.move_stop.left = 0;
-        this.flags.move_stop.right = 0;
+        _.each(this.keyboard.flag, function(key) {
+            switch(key.keyCode) {
+                case this.keyboard.keys['W']:
+                    this.trigger('player_move', 'up'); break;
 
+                case this.keyboard.keys['A']:
+                    this.trigger('player_move', 'left'); break;
+
+                case this.keyboard.keys['S']:
+                    this.trigger('player_move', 'down'); break;
+
+                case this.keyboard.keys['D']:
+                    this.trigger('player_move', 'right'); break;
+
+                case this.keyboard.keys['arrow up']:
+                    this.trigger('player_direct', 'up'); break;
+
+                case this.keyboard.keys['arrow down']:
+                    this.trigger('player_direct', 'left'); break;
+
+                case this.keyboard.keys['arrow left']:
+                    this.trigger('player_direct', 'down'); break;
+
+                case this.keyboard.keys['arrow right']:
+                    this.trigger('player_direct', 'right'); break;
+            }
+
+            if (key.free) {
+                delete(this.keyboard.flag[''+key.keyCode]);
+            }
+
+        }, this);
     },
 
     timer: null,
